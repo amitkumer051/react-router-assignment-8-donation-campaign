3050511780cbc8984df2f7712601e7ca3efb3c37
amitkumer051 <amitkumer051@gmail.com>
2023-09-26 01:00:25 +0600

    added cards individual color and perfection

--- src/componants/Card/Card.jsx
+++ src/componants/Card/Card.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable react/prop-types */
 
 const Card = ({ card }) => {
+    const { id, img, title, bg_color,font_bg,font_color, category } = card;
 
     return (
-        <div className="card-compact w-60 bg-base-100 shadow-md border">
-            <figure><img className="w-full h-40" src={card.img} alt="" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>{card.title}</p>
-                <div className="">
-                    <button className="btn btn-primary">{card.category}</button>
-                </div>
+        <div>
+            <div style={{background:bg_color}} className="rounded-lg w-60 mx-auto">
+            <figure><img className="w-full h-40 rounded-t-lg" src={img} alt="" /></figure>
+            <div className=" p-3">
+                <h2 style={{color:font_color,background:font_bg}}  className="text-xs text-center items-center  p-[2px] w-20 rounded-md">{category}</h2>
+                <p style={{color:font_color}} className="text-base font-bold">{title}</p>
             </div>
         </div>
-    );
+        </div>
+    ); 
 };
 
 export default Card;
--- src/componants/Cards/Cards.jsx
+++ src/componants/Cards/Cards.jsx
@@ -4,7 +4,7 @@ import Card from "../Card/Card";
 const Cards = ({cards}) => {
     // console.log(cards);
     return (
-        <div className="grid lg:grid-cols-4 gap-4  md:grid-cols-2 grid-cols-1 mt-10">
+        <div className="grid lg:grid-cols-4 gap-6 mb-6  md:grid-cols-2 grid-cols-1 mt-10">
             {
                 cards.map(card => <Card key={card.id} card={card}></Card>)
             }
