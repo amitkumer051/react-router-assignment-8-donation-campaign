2a89a2b0f0820c2fdf5b520a2ab18f8c7ef06de4
amitkumer051 <amitkumer051@gmail.com>
2023-09-27 23:42:29 +0600

    test commit

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
     children:[
       {
         path:'/',
-        loader:()=> fetch('/data.json'),
+        loader:()=> fetch('./data.json'),
         element: <Home></Home>
       },
       {
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
       {
         path:'/cards/:id',
         element: <DetailsOfCards></DetailsOfCards>,
-        loader:()=> fetch('/data.json')
+        loader:()=> fetch('./data.json')
       }
     ]
   },
